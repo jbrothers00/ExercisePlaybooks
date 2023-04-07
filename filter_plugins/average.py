96b401abf469448f526c69f0082690a2e99fbd01
jbrothers00 <jbrothers0028@gmail.com>
2023-04-07 16:47:09 +0530

    Second Commit

---
+++ filter_plugins/average.py
@@ -0,0 +1,8 @@
+def average(list):
+    return sum(list)/ float(len(list))
+
+class FilterModule(object):
+    def filters(self):
+        return {
+                'average': average
+                }
